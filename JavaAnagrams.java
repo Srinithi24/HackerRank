004ef30151d300891f821ca73b2be4753324f205
Srinithi24 <110291433+Srinithi24@users.noreply.github.com>
2025-05-21 16:16:57 +0530

    Create JavaAnagrams.java

---
+++ JavaAnagrams.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class JavaAnagrams {
+
+    static boolean isAnagram(String a, String b) {
+        
+        a.toLowerCase();
+        b.toLowerCase();
+        if(a.length() != b.length()){
+            return false;
+        }
+        
+        int[] count = new int[26];
+        for(int i=0; i<a.length(); i++){
+            count[a.charAt(i)-'a']++;
+            count[b.charAt(i) - 'a']--; 
+        }
+        for(int c: count){
+            if(c != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+  public static void main(String[] args) {
+    
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        scan.close();
+        boolean ret = isAnagram(a, b);
+        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+    }
+}
